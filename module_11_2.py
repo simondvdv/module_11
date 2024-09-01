f2b0a7bba4b4bd134fbf6bf98f5055c14023e51e
simondv4 <simondv4@gmail.com>
2024-09-01 20:26:46 +0300

    init

--- module_11_2.py
+++ module_11_2.py
@@ -16,12 +16,13 @@ def intersept(obj):
     obj_type = type(obj)
     attr_2 = [i for i in dir(c1) if not callable(getattr(c1, i))]
     methods_2 = [i for i in dir(c1) if callable(getattr(c1, i))]
-    module_ = obj.__module__
+    module_ = obj.__class__.__module__
     return {'type': obj_type, 'attributes': attr_2, 'methods': methods_2, 'module': module_}
 
 
 c1 = compl_nubmer(1, 3)
 print(intersept(c1))
+print(intersept(5))
 
 
 
